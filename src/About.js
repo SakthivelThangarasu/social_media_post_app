19e6519766b00db0e2acae145b4ee4e912d1e92e
SakthivelThangarasu <141846876+SakthivelThangarasu@users.noreply.github.com>
2023-11-20 18:48:37 +0530

    Add files via upload

---
+++ src/About.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <main className='About'>
+        <h2>About</h2>
+        <p sytle={{ marginTop:"1rem" }}>
+          Hi, I am Sakthivel. Together lets make education free for everyone in the world. 
+        </p>
+    </main>
+  );
+};
+
+export default About;
